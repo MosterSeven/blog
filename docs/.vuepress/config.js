cfd3d3877bcacacc45596b77e69746448fc63772
MosterSeven <1144813094@qq.com>
2022-07-10 19:12:17 +0800

    change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    title: 'bulk java',
-    description: 'Just playing around',
-    base: '/vuepress-demo-deploy-gitee/',
-    themeConfig: {
-        sidebar: 'auto',
-        nav: [
-            {text: 'Home', link: '/'},
-            {text: 'Guide', link: '/guide/'},
-            {text: 'Gitee', link: 'https://www.gitee.com'},
-        ]
-    },
-}
+  title: "bulk java",
+  description: "Just playing around",
+  base: "/blog/",
+  themeConfig: {
+    sidebar: "auto",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "Gitee", link: "https://www.gitee.com" },
+    ],
+  },
+};
