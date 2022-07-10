d3cf8527ee2452589e3a7576d10f0243460981a7
MosterSeven <1144813094@qq.com>
2022-07-10 19:03:44 +0800

    d

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'bulk java',
+    description: 'Just playing around',
+    base: '/vuepress-demo-deploy-gitee/',
+    themeConfig: {
+        sidebar: 'auto',
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: 'Gitee', link: 'https://www.gitee.com'},
+        ]
+    },
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /bulk.png
+heroText: Bulk java
+tagline: 你要的轮子就在这里啊
+actionText: 浅浅看一下 →
+actionLink: /guide/
+features:
+- title: part1
+  details: 各个组件独立开发，除了基础公共部分，相互独立。
+- title: part2
+  details: 每个组件是单个具体功能的实现CRUD。
+- title: part3
+  details: 组件可搭配组装，组成模块或项目。
+  footer: MIT Licensed | Copyright © 2022-present bulk java
+---
